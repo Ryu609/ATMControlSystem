4ed9b95a8d98f86bb22d89ca4be60504e9a321d4
Amardev Jonauth <m.joynauth@gmail.com>
2015-11-12 09:31:15 +0400

    ReportedCard Codes

---
+++ ATMControlSystem/ATMControlSystem.Tests/Utilities/LoginUtilsTest.cs
@@ -0,0 +1,24 @@
+﻿using ATMControlSystem.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMControlSystem.Tests.Utilities
+{
+    [TestClass]
+   public class LoginUtilsTest
+    {
+        [TestMethod]
+        public void IsCardReported()
+        {
+            var expectedResult = false;
+            var actualResult = LoginUtils.IsCardReported(00000123456);
+
+            Assert.AreEqual<bool>(expectedResult, actualResult);
+
+        }
+    }
+}
